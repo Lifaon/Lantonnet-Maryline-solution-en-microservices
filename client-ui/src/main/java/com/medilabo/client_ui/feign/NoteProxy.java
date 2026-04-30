755e66488db2b719d25214aa83a6ad0955b75ff7
Maryline Lantonnet <maryline.lantonnet@gmail.com>
2026-04-30 21:47:31 +0200

    Remove usage of Eureka in client

--- client-ui/src/main/java/com/medilabo/client_ui/feign/NoteProxy.java
+++ client-ui/src/main/java/com/medilabo/client_ui/feign/NoteProxy.java
@@ -3,6 +3,6 @@ package com.medilabo.client_ui.feign;
 import com.medilabo.note.api.NoteApi;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name = "note-microservice")
+@FeignClient(name = "note-client", url = "${gateway.url}/note-microservice")
 public interface NoteProxy extends NoteApi {
 }
--- client-ui/src/main/java/com/medilabo/client_ui/feign/PatientProxy.java
+++ client-ui/src/main/java/com/medilabo/client_ui/feign/PatientProxy.java
@@ -3,6 +3,6 @@ package com.medilabo.client_ui.feign;
 import com.medilabo.patient.api.PatientApi;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name = "patient-microservice")
+@FeignClient(name = "patient-client", url = "${gateway.url}/patient-microservice")
 public interface PatientProxy extends PatientApi {
 }
